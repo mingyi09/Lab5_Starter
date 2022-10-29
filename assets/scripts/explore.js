c1cf4e9c8f828bdb03728166d819de7f020894dc
mingyi09 <mingyili09@aliyun.com>
2022-10-29 10:36:37 -0700

    Finish horn and speech

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,69 @@
 // explore.js
 
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+
 window.addEventListener('DOMContentLoaded', init);
 
+
+let voices = [];
+const talkFace = document.querySelector("img");
+
 function init() {
-  // TODO
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  talkContent();
+}
+
+function populateVoiceList(){
+  voices = synth.getVoices();
+  
+  const selectedInd = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = "";
+
+  for (let i=0; i<voices.length; i++){
+    const newOption = document.createElement('option');
+    newOption.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      newOption.textContent += ' -— DEFAULT';
+    }
+
+    newOption.setAttribute('data-lang', voices[i].lang);
+    newOption.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(newOption);
+  } 
+  voiceSelect.selectedIndex = selectedInd;
+}
+
+function talkContent(){
+  let content = document.querySelector("textarea");
+  const talkButton = document.querySelector("button");
+  
+
+  talkButton.addEventListener('click',()=>{
+    let utterThis = new SpeechSynthesisUtterance(content.value);
+    const selectedOption = voiceSelect[voiceSelect.selectedIndex].getAttribute('data-name');
+    
+    for (let i=0; i<voices.length;i++){
+      if (voices[i].name == selectedOption){
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+
+    synth.speak(utterThis);
+    if (window.speechSynthesis.speaking){
+      talkFace.src="assets/images/smiling-open.png";
+    }
+
+    const interval = setInterval(function(){
+      if (!window.speechSynthesis.speaking){
+        talkFace.src="assets/images/smiling.png";
+        clearInterval(interval);
+      }
+    },500);
+  }) 
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,59 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+const jsConfetti_object = new JSConfetti();
 
 function init() {
-  // TODO
+  changeImg();
+  changeSound();
 }
+
+function changeImg(){
+  let select_img = document.getElementById("horn-select");
+
+  select_img.addEventListener('change', (event) => {
+    let new_img = document.querySelector("img");
+    let new_img_name = event.target.value;
+    new_img.src=`assets/images/${new_img_name}.svg`;
+    document.querySelector("audio").src=`assets/audio/${new_img_name}.mp3`;
+  });
+}
+
+function changeSound(){
+  let play_button = document.querySelector("button");
+  let vol_img = document.querySelector("img[src='assets/icons/volume-level-2.svg']");
+  let vol_ctrl = document.querySelector("div>input");
+  let sound = document.querySelector("audio");
+
+  vol_ctrl.addEventListener('change',(event)=>{
+    let sound_level = event.target.value;
+    sound.volume = sound_level/100;
+  
+    if (sound_level==0){
+      vol_img.src="assets/icons/volume-level-0.svg";
+    }
+    else if (sound_level>=1 && sound_level<33){
+      vol_img.src="assets/icons/volume-level-1.svg";
+    }
+    else if (sound_level>=33 && sound_level<67){
+      vol_img.src="assets/icons/volume-level-2.svg";
+    }
+    else {
+      vol_img.src="assets/icons/volume-level-3.svg";
+    }
+    }
+  )
+
+  play_button.addEventListener('click',()=>{
+    if(sound.src.indexOf("party-horn")!=-1){
+      jsConfetti_object.addConfetti({
+        emojis: ['🦄','🌈', '✨', '💫', '🌸'],
+        confettiRadius:80,
+      });
+    }
+    sound.play();
+  })
+}
+
+
+
